9128bf92db6ff626fb2605f8c3151518144a1f18
thernandez30 <tornadoteresa77@gmail.com>
2025-10-30 13:43:15 +0000

    more work

--- week11_day1.py
+++ week11_day1.py
@@ -22,12 +22,16 @@ print(first_name.upper())
 print(len(first_name))
 
 # 3. Combine your name and favorite color into one message
-
+print(f"{first_name} and {favorite_color}")
 
 #  Step 3: Math Practice
 # Use arithmetic operators with your favorite number
-
-
+num1 = 3
+num2 = 3
+num3 = int(input("what is your 3rd favorite number")) #5
+num4 = int(input("what is your 4th favorite number"))#4
+print(num1 + num2) # output 6
+print(num3 + num4) #output 9 (it would've been 54 if you didn)
 #  Step 4: User Input Practice
 # Ask the user two questions and combine answers
 
