eb84c4ce04de5c0db0cf2a9d62f54d1897478f45
thernandez30 <tornadoteresa77@gmail.com>
2025-10-30 13:33:02 +0000

    online

---
+++ week11_day1.py
@@ -0,0 +1,39 @@
+# open up the vscode from previous day
+# create a new file called: week11_day1.py
+# ----------------------------------------
+# Day 2 Review Activity: My Personal Info Generator
+# ----------------------------------------
+
+print(" Welcome to the Python Review Activity!")
+print("You’ll review variables, strings, numbers, and print formatting.\n")
+
+# Step 1: Create Variables
+# TO DO: Replace the values below with your own info
+first_name = "Teresa"
+age = 15
+favorite_color = "Blue"
+favorite_number = 13
+
+#  Step 2: Practice String Operations
+# 1. Print your name in uppercase
+print(first_name.upper())
+
+# 2. Print how many letters are in your name
+print(len(first_name))
+
+# 3. Combine your name and favorite color into one message
+
+
+#  Step 3: Math Practice
+# Use arithmetic operators with your favorite number
+
+
+#  Step 4: User Input Practice
+# Ask the user two questions and combine answers
+
+
+
+
+# ⚙️ Step 5: Final Challenge (combine it all)
+# Use math and strings together
+
